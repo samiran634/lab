977cd33d9a7bb37a289412580031223eaea3fd68
samiran634 <samiranchakraborty2006@gmail.com>
2024-09-03 20:16:48 +0530

    first commit

---
+++ Q4.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a linked list node
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// Define the structure for a stack
+typedef struct Stack {
+    Node* top;
+} Stack;
+
+// Function to create a new node
+void insertAtEnd(Node** head, int data) {
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        Node* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+       
+    }
+}
+Node* createNode(int data) {
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Memory error\n");
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to initialize a stack
+void initStack(Stack* stack) {
+    stack->top = NULL;
+}
+
+// Function to check if the stack is empty
+int isEmpty(Stack* stack) {
+    return stack->top == NULL;
+}
+
+// Function to push an element onto the stack
+void push(Stack* stack, int data) {
+    Node* newNode = createNode(data);
+    if (isEmpty(stack)) {
+        stack->top = newNode;
+    } else {
+        newNode->next = stack->top;
+        stack->top = newNode;
+    }
+}
+
+// Function to pop an element from the stack
+int pop(Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    int data = stack->top->data;
+    Node* temp = stack->top;
+    stack->top = stack->top->next;
+    free(temp);
+    return data;
+}
+
+// Function to get the top element of the stack
+int top(Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    return stack->top->data;
+}
+
+int main() {
+    Stack stack;
+    initStack(&stack);
+ int n;
+ printf("%d",&n);
+ for(int i=0;i<n;i++){
+    printf("enter stack elements :");
+    int p;
+    scanf("%d",&p);
+    insertAtEnd(&stack,p);
+ }
+
+    printf("Top element: %d\n", top(&stack));
+    printf("Popped element: %d\n", pop(&stack));
+    printf("Top element: %d\n", top(&stack));
+
+    return 0;
+}
